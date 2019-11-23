05ccdf7fbf197aaa1afaf3192a28d77ca2a328d1
LuizRamos <luizramospe@hotmail.com>
2019-11-23 01:20:20 -0500

    custom data decoding

--- RESThub/Gist.swift
+++ RESThub/Gist.swift
@@ -12,10 +12,19 @@ struct Gist: Codable {
 
     var id : String
     var isPublic: Bool
+    var description: String
 
     enum CodingKeys: String, CodingKey {
         case id
+        case description
         case isPublic = "public"
     }
-    
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: CodingKeys.id)
+        self.isPublic = try container.decode(Bool.self, forKey: CodingKeys.isPublic)
+        self.description = try container.decodeIfPresent(String.self, forKey: CodingKeys.description) ?? "Description is nil"
+        
+    }
 }
